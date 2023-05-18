b684a13fa577651be440b8d420e1fd84ee28b3dd
andrewhemmaplardh <83370682+andrewhemmaplardh@users.noreply.github.com>
2023-05-18 16:58:29 +0000

    Create new (unimplemented) dependency for image

---
+++ backend/lib/image.js
@@ -0,0 +1,10 @@
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY
+
+async function getDefaultImage(event, metadata) {
+    // TODO IMPLEMENT ME
+    return "";
+}
+
+module.exports = {
+  getDefaultImage,
+}
