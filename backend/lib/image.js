4cbcb15ee41b9e60d82ada936834ccacfdbaad40
andrewhemmaplardh <83370682+andrewhemmaplardh@users.noreply.github.com>
2023-05-19 16:20:05 +0000

    First draft of OpenAI image fetching

--- backend/lib/image.js
+++ backend/lib/image.js
@@ -1,8 +1,14 @@
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY
 
-async function getDefaultImage(event, metadata) {
+async function getDefaultImage(title) {
     // TODO IMPLEMENT ME
-    return "";
+    const response = await openai.createImage({
+        prompt: title,
+        n: 1,
+        size: "256x256",
+    });
+    imageUrl = response.data.data[0].url;
+    return imageUrl;
 }
 
 module.exports = {
