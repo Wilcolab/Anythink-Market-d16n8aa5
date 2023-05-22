085b19b64e7bcb18734e0b9a218de39bbc5e6b29
andrewhemmaplardh <83370682+andrewhemmaplardh@users.noreply.github.com>
2023-05-22 20:35:11 +0000

    add draft of test to verify OpenAPI image fetching

--- tests/e2e/concurrent/items.test.js
+++ tests/e2e/concurrent/items.test.js
@@ -4,8 +4,11 @@ const {
   describe,
   beforeEach,
   test,
+  jest
 } = require("@jest/globals");
+
 const { AnythinkClient } = require("../anytinkClient");
+const { getDefaultTitle, getDefaultImage } = require("../../../backend/lib/image");
 const {
   randomItemInfo,
   randomUserInfo,
@@ -52,11 +55,14 @@ describe("Items Route", () => {
     });
 
     test("Can create item without image", async () => {
+      const STUB_IMAGE = "stub image";
+      jest.mock("../../../backend/lib/image");
+      getDefaultImage.mockResolvedValue(STUB_IMAGE);
       const createdItem = await anythinkClient.createItem(
         randomItemInfo({ image: undefined }),
         user
       );
-      expect(createdItem.image).toBeFalsy();
+      expect(createdItem.image).toBeTruthy();
       expect(createdItem.slug).toBeDefined();
     });
 
