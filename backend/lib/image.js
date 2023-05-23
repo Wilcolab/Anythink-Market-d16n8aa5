b49f846f84c526a6045b4b3a072083380c655fa9
andrewhemmaplardh <83370682+andrewhemmaplardh@users.noreply.github.com>
2023-05-23 03:21:52 +0000

    remove some debug logs

--- backend/lib/image.js
+++ backend/lib/image.js
@@ -5,16 +5,12 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 async function getDefaultImage(title) {
-    console.log("DR. STRANGEPORK ENTERING getDefaultImage");
     const response = await openai.createImage({
         prompt: title,
         n: 1,
         size: "256x256",
     });
-    console.log("DR. STRANGEPORK ABOUT TO FISH OUT HIS IMAGE URL");
     imageUrl = response.data.data[0].url;
-    console.log("DR. STRANGEPORK GOT AN IMAGE URL: " + imageUrl);
-    console.log("DR. STRANGEPORK ABOUT TO LEAVE DA FUNKTION");
     return imageUrl;
 }
 
